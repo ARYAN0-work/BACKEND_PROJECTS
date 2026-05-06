8ae10f79e2b26b921c81350f0c84700d138a1769
ARYAN0-WORK <carrer1270@gmail.com>
2026-05-06 20:38:13 +0000

    adding controllers for our basic routes

--- task-manager/controllers/task.js
+++ task-manager/controllers/task.js
@@ -1,7 +1,23 @@
 const getAllTask = (req,res)=>{
-    res.send('all items from the file')
+    res.send('get all task')
+}
+
+const createTask =(req,res)=>{
+    res.send('create task')
+}
+
+const getTask = (req,res)=>{
+    res.send('get single task')
+}
+
+const deletetTask = (req,res)=>{
+    res.send('delete task')
+}
+
+const updateTask = (req,res)=>{
+    res.send('update task')
 }
 
 module.exports = {
-    getAllTask
+    getAllTask,createTask,getTask,deletetTask,updateTask
 }
