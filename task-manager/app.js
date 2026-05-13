77e731e664da7f91206012c3f6cacd5ca8da89da
ARYAN0-WORK <carrer1270@gmail.com>
2026-05-13 11:49:12 +0000

    fix(bug):fixing basic bugs

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,16 +1,18 @@
+require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/task')
 
 //middleware
 app.use(express.json())// if we dont use this it wont be in the req.body 
+// here you forget ()
 
 // routes 
 app.get('/hello',(req,res)=>{
     res.send('Task Manger App')
 })
 
-app.use('api/v1/tasks',tasks)
+app.use('/api/v1/tasks',tasks)// here you forget /api
 
 const PORT = 3000
 
