899b9c05cb3c40fc5b79403098966f04862878cc
ARYAN0-WORK <carrer1270@gmail.com>
2026-06-19 14:11:12 +0000

    setting up logic for query string parameter

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,18 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req,res)=>{
-    //const products = await Product.find({}) for all products
-    const products = await Product.find({
-        //fetured:true
-        name:'vase table'
-    })// if you want something specfic 
+    const products = await Product.find({name:'vase table'})
     res.status(200).json({ products,nbHits:products.length })
 }
 
-
 const getAllProducts = async(req,res)=>{
-    console.log(req.query)
-    res.status(200).json({ msg:'products route' })
+    const rpoducts = await Product.find(req.query)
+    res.status(200).json({  products,nbHits:products.length  })
 }
 
 module.exports ={
