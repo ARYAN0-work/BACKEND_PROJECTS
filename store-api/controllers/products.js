1f14805df5011d3814d5d677d6930f747a5eaf9d
ARYAN0-WORK <carrer1270@gmail.com>
2026-06-20 23:05:19 +0000

    setting up the pattern for all the queryOnjects

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,19 +1,23 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req,res)=>{
-    const products = await Product.find({name:'vase table'})
+    const products = await Product.find({name:'albany'})
     res.status(200).json({ products,nbHits:products.length })
 }
 
 const getAllProducts = async(req,res)=>{
-    const products = await Product.find(req.query)// what happens we mongoose look and say i dont have something page property and set to 2 => in short it will work with the most basic setup but as with filtering increase more complexity it will have better approch such as only the prop you want to apply 
-   const { featured } = req.query// i am only looking for feature
-
-// method to setup a new property  => in short if fature is true => from this we can avoid that bug which come in starting 
+    const products = await Product.find(req.query)
+   const { featured,company,name } = req.query
    const queryObject = {}
 
    if (featured) {
     queryObject.featured = featured === 'true'? true:false
+   }
+   if (company){
+    queryObject.company = company
+   }
+   if (name) {
+    queryObject.name = name // we can search a paticular name empror bed 
    }
    console.log(queryObject)
 
