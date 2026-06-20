cf23bce60145bc5eeaa35c0e6395f42ac93398d4
ARYAN0-WORK <carrer1270@gmail.com>
2026-06-20 22:33:40 +0000

    fixing bugs

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -6,8 +6,9 @@ const getAllProductsStatic = async(req,res)=>{
 }
 
 const getAllProducts = async(req,res)=>{
-    const rpoducts = await Product.find(req.query)
-    res.status(200).json({  products,nbHits:products.length  })
+    //const products = await Product.find(req.query)
+    console.log(req.query)
+    res.status(200).json({msg:'products route'})
 }
 
 module.exports ={
