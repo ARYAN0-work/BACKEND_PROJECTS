fa4a6682a3ac854a293438b57f60382ad93bde52
ARYAN0-WORK <carrer1270@gmail.com>
2026-06-26 01:13:29 +0000

    setting up jwt

--- jwt-basics/controllers/main.js
+++ jwt-basics/controllers/main.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
-const login = async(req,res)=>{
+// check username , password in post(login) request
+// if exist create new JWt
+// send back to frontend
+// setup auth so only the req wuth jwt access the dashboard
+
+const login = async(req,res)=>{    
+    const {username,password} = req.body
+    console.log(username,password);
     res.send('Fake Login/Register/Signup Route')
 }
 
 const dashboard = async (req,res) => {
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello, John Doe`,secret:`Here is your authorized data your lucky number is ${luckyNumber}`})
-}// this is where we are gonna share the secret or authorized data 
+}
 
 module.exports ={
     login,dashboard
