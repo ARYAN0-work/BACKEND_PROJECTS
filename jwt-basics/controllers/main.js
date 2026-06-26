28078bcd24ea1b45debaf3c35a41f2aa4ede8855
ARYAN0-WORK <carrer1270@gmail.com>
2026-06-26 01:54:35 +0000

    signing up a token and sending it back to front-end now

--- jwt-basics/controllers/main.js
+++ jwt-basics/controllers/main.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const CustomAPIError = require('../errors/custom-error')
+const jwt =require('jsonwebtoken')
 
 const login = async(req,res)=>{    
-    const {username,password} = req.body
-
-    
-    // WITH DB WE HAVE 3 OPTIONS 
-    // mongo required validation 
-    // entire additional layer of validation=> use  joi 
-    // check both the balues 
+    const {username,password} = req.body// dont send a confidential infoover here dont send password bad practice
+ 
     if (!username|| !password) {
         throw new CustomAPIError('Plz provide email and password',400)
     }
 
-    console.log(username,password);
-    res.send('Fake Login/Register/Signup Route')
+    //just for demo, normally provided byy DB !!!
+    const id = new Date().getDate()
+
+    // try to keep payload small, better experience for user 
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})// payload | jwt secret |options 
+
+    res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async (req,res) => {
