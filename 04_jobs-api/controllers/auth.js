a55cc94d8fff8f297c3ce8ced75e1dc4b256dd5e
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-01 11:55:01 +0000

    learning mongoose validation

--- 04_jobs-api/controllers/auth.js
+++ 04_jobs-api/controllers/auth.js
@@ -1,10 +1,17 @@
 const User =require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+const {BadRequestError} = require('../errors')
 
 const register =async(req,res)=>{
+        const {name,email,password} =req.body
+        if (!name||!email||!password) {
+           throw new BadRequestError('plz provide all three')            
+        }
+
+
     const user = await User.create({ ...req.body })
     res.status(StatusCodes.CREATED).json({ user })
-}
+}// very bad practice to save our password as a string
 
 const login =async(req,res)=>{
     res.send('login user')
